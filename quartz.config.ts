b78b34b306b3a885cacf109e3f065756246f5247
KeiSoda <geekacao@gmail.com>
2025-02-08 15:29:02 -0300

    Quartz sync: Feb 8, 2025, 3:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "Jujutsu RPG",
+    pageTitleSuffix: "Jujutsu RPG",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "pt-BR",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
